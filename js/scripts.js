8cf64b4aa0d483c04003a5411d432731a4cbf6a4
Gladinette Monzon <gladinette35@gmail.com>
2021-11-14 13:41:58 +0800

    Paid revision

--- js/scripts.js
+++ js/scripts.js
@@ -212,50 +212,74 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
         
-        if ($('input[name=attendance]:checked').length == 0) {
+        // if attending, inputs are mandatory
+        // otherwise, optional
+        if ( ($('#radio-a').is(":checked") 
+                && (   $('#phone').val()       == "" 
+                    || $('#extras').val()      == "" 
+                    || $('#invite_code').val() == "")
+                ) 
+             || ($('input[name=attendance]:checked').length == 0)
+           ){
             $('#alert-wrapper').html(alert_markup('danger', '<strong>All fields are required!</strong>'));
             exit();
-        } else {
-            if (MD5($('#invite_code').val()) !== 'e3251075554389fe91d17a794861d47b'
+        }
+        
+        // if attending, validate invite code
+        if ($('#radio-a').is(":checked")) {
+            if (   MD5($('#invite_code').val()) !== 'e3251075554389fe91d17a794861d47b'
                 && MD5($('#invite_code').val()) !== '38181d991caac98be8fb2ecb8bd0f166'
                 && MD5($('#invite_code').val()) !== '13d63838ef1fb6f34ca2dc6821c60e49'
                 && MD5($('#invite_code').val()) !== 'e2c4a40d50b47094f571e40efead3900'
                 && MD5($('#invite_code').val()) !== 'ecb287ff763c169694f682af52c1f309'
                 && MD5($('#invite_code').val()) !== '5d50d22735a7469266aab23fd8aeb536'
-                ) {
+            ) {
                 $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+                exit();
             } else {
                 if (MD5($('#invite_code').val()) === 'e3251075554389fe91d17a794861d47b' && $('#extras').val() > 1) {
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Only 1 companion is allowed.'));
+                    exit();
                 } else if  (MD5($('#invite_code').val()) === '38181d991caac98be8fb2ecb8bd0f166' && $('#extras').val() > 2) {
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Only up to 2 companions is allowed.'));
+                    exit();
                 } else if  (MD5($('#invite_code').val()) === '13d63838ef1fb6f34ca2dc6821c60e49' && $('#extras').val() > 3) {
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Only up to 3 companions is allowed.'));
+                    exit();
                 } else if  (MD5($('#invite_code').val()) === 'e2c4a40d50b47094f571e40efead3900' && $('#extras').val() > 4) {
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Only up to 4 companions is allowed.'));
+                    exit();
                 } else if  (MD5($('#invite_code').val()) === 'ecb287ff763c169694f682af52c1f309' && $('#extras').val() > 5) {
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Only up to 5 companions is allowed.'));
+                    exit();
                 } else if  (MD5($('#invite_code').val()) === '5d50d22735a7469266aab23fd8aeb536' && $('#extras').val() > 6) {
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Only up to 6 companions is allowed.'));
-                } else {
-                    $.post('https://script.google.com/macros/s/AKfycbweXBBtMptgNdceL4EX_uhMqCRdYwtNzdmXmBYbjFZ94pmw_H9WjOHueAXuLypY38rpmA/exec', data)
-                        .done(function (data) {
-                            console.log(data);
-                            if (data.result === "error") {
-                                $('#alert-wrapper').html(alert_markup('danger', data.message));
-                            } else {
-                                $('#alert-wrapper').html('');
-                                $('#rsvp-modal').modal('show');
-                                document.getElementById('rsvp-form').reset();
-                            }
-                        })
-                        .fail(function (data) {
-                            console.log(data);
-                            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                        });
+                    exit();
                 }
             }
         }
+
+        $.post('https://script.google.com/macros/s/AKfycbweXBBtMptgNdceL4EX_uhMqCRdYwtNzdmXmBYbjFZ94pmw_H9WjOHueAXuLypY38rpmA/exec', data)
+            .done(function (data) {
+                console.log(data);
+                if (data.result === "error") {
+                    $('#alert-wrapper').html(alert_markup('danger', data.message));
+                } else {
+                    $('#alert-wrapper').html('');
+
+                    // attending
+                    if ($('#radio-a').is(":checked")) {
+                        $('#rsvp-modal').modal('show');
+                    } else { // not attending
+                        $('#rsvp-modal2').modal('show');
+                    }
+                    document.getElementById('rsvp-form').reset();
+                }
+            })
+            .fail(function (data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            });
     });
 
     function getUrlVars() {
